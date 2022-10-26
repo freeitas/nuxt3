bdc1a775387246e69d9777b5f96c8231043b654a
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-10-26 14:42:18 -0300

    feat: added course page

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Lesson</h2>
+  <p>{{ chapter.title }}</p>
+  <p>{{ lesson.title }}</p>
+</template>
+
+<script setup>
+const course = useCourse()
+const route = useRoute()
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  )
+})
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  )
+})
+</script>
