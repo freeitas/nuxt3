405d15c88c5ae85f274bb62f756ad25bccff149c
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-10-30 15:48:05 -0300

    feat: added layout

--- pages/course.vue
+++ pages/course.vue
@@ -1,7 +1,4 @@
 <template>
-  <div
-    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
-  >
     <div class="prose mb-12">
       <h1>
         <span class="font-medium">
@@ -46,7 +43,6 @@
         <NuxtPage />
       </div>
     </div>
-  </div>
 </template>
 
 <script setup>
