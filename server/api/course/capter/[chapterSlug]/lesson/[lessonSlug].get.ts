bc5a08f2a03148ee68b043cd8c9947b82f148512
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-12-21 22:14:58 -0300

    feat: typed endpoint

--- server/api/course/capter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/capter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,9 +1,17 @@
+import {
+  Course,
+  Chapter,
+  Lesson,
+  LessonWithPath,
+} from '~/types/course';
 import course from '~/server/courseData';
 
-export default defineEventHandler((event) => {
+course as Course;
+
+export default defineEventHandler((event): LessonWithPath => {
   const { chapterSlug, lessonSlug } = event.context.params;
 
-  const chapter = course.chapters.find(
+  const chapter:Maybe<Chapter> = course.chapters.find(
     (chapter) => chapter.slug === chapterSlug
   );
 
@@ -14,7 +22,7 @@ export default defineEventHandler((event) => {
     });
   }
 
-  const lesson = chapter.lessons.find(
+  const lesson:Maybe<Lesson> = chapter.lessons.find(
     (lesson) => lesson.slug === lessonSlug
   );
 
@@ -25,5 +33,8 @@ export default defineEventHandler((event) => {
     });
   }
 
-  return lesson;
+  return {
+    ...lesson,
+    path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}`,
+  };
 });
